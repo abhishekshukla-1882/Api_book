c7cc72089a7d0562dd3cfca36600fdb477507953
abhishekshukla <abhishekshukla2022jan@cedcoss.com>
2022-04-06 11:29:34 +0530

    google

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends Controller
         if(isset($_POST['submit'])){
             $val = $this->request->getPost('val');
             // echo $val;
+            $val = urlencode($val);
 
         }
         if($val){
@@ -19,16 +20,22 @@ class IndexController extends Controller
             $val = 'fantastic';
         }
         $url = "https://openlibrary.org/search.json?q=$val";
+        // $url = 'http://httpbin.org/post';
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
+
+        // curl_setopt($ch, CURLOPT_URL,$url);
+        // curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS,
+        //             "postvar1=value1&postvar2=value2&postvar3=value3");
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
  
         $book = json_decode($response)->docs;
         // echo "<pre>";
-        // print_r($book);
+        // print_r($response);
         // die;
 
         $this->view->book = $book;
@@ -40,8 +47,9 @@ class IndexController extends Controller
             $val =urlencode($this->request->getPost('title'));
             // print_r( $val->full_title);
             // die;
+            // https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=details&format=json
             $url = "https://openlibrary.org/api/books?bibkeys=ISBN:$val&jscmd=details&format=json";
-        //    $url ="https://openlibrary.org/search.json?q=$val&mode=ebooks&has_fulltext=true";
+            //    $url ="https://openlibrary.org/search.json?q=$val&mode=ebooks&has_fulltext=true";
            $ch = curl_init();
 
            //grab URL and pass it to the variable.
@@ -49,10 +57,10 @@ class IndexController extends Controller
 
            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
            $response = curl_exec($ch);
-           echo $response,"<br>  hhhhh";
+        //    print_r($response,"<br>  hhhhh");
            $book = ((array)json_decode($response));
         //    echo "<pre>";
-        //    print_r($book["ISBN:$val"]);
+        //    print_r($book);
         //    die;
             $this->view->book = $book["ISBN:$val"];
             $this->view->val = $val;
